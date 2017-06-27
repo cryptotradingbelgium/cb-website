f3442e36b40c6783657a5c0bac8a20e5dacea742
amnesiacu <jonas.vandevyver@gmail.com>
2017-06-27 21:24:55 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let express     = require('express');
+let path        = require('path');
+let morgan      = require('morgan');
+let app         = express();
+
+const port      = process.env.PORT || 4000;
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+});
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/public', '404.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
