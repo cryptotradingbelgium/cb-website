037173d5fa31918a057e621cf24c45db9b888762
amnesiacu <jonas.vandevyver@gmail.com>
2017-06-28 22:53:42 +0200

    Add responsiveness, {about, contact, sitemap} - pages

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'about.html'));
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'contact.html'));
+});
+app.get('/sitemap', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'sitemap.html'));
+});
+
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, '/public', '404.html'));
 });
