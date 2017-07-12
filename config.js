4d5420bb9da4308c349f143e45c9734ecb22ad08
tomploem <tom.ploem@gmail.com>
2017-07-12 18:30:04 +0200

    updated intro-section

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.slack = {
+    'token' : 'xoxp-187034286548-190651235799-211779882613-e31b1b20b610833ce63c34badae8edcf',
+    'pretty' : 1
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -5,11 +5,22 @@ let path        = require('path');
 let morgan      = require('morgan');
 let app         = express();
 
+let request     =    require('request');
+let config      =   require('./config');
+
 const port      = process.env.PORT || 4000;
 
 app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+/*
+     BodyParser is middleware that intercepts the request and parsing our passed data into JSON.
+* */
+let bodyParser  =    require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views', 'index.html'));
@@ -23,6 +34,36 @@ app.get('/contact', (req, res) => {
 });
 app.get('/sitemap', (req, res) => {
     res.sendFile(path.join(__dirname, '/views', 'sitemap.html'));
+});
+
+
+app.post('/join', (req, res) => {
+
+    // By using bodyParser we can easily extract variables from requests
+    var email = req.body.email;
+
+    var email_param = '&email=' + email;
+    var token_param = 'token=' + config.slack.token;
+
+    var url = 'https://slack.com/api/users.admin.invite?'+ token_param + email_param
+
+    request.post(url,function(error,response) {
+        if (error) {
+            if (res.statusCode !== 200){
+                console.log('error')
+                console.log(error)
+            } //etc
+        }
+
+        if (response.body.ok){
+            res.send(200, 'Check your email!')
+        }else{
+            res.send(500, 'Something went wrong!')
+        }
+
+    });
+
+    res.sendFile(path.join(__dirname, '/views', '404.html'));
 });
 
 app.get('/*', (req, res) => {
